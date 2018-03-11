50447d2d577d121a6bd405bcc1c53f029a660ff0
Edlaine Zamora <ezamora@thoughtworks.com>
2018-03-11 16:05:10 -0300

    ISSUE-8 created login default screen

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Constants} from 'expo';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import LoginScreen from './src/components/LoginScreen.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Login!</Text>
+        <MobileFlashCardStatusBar backgroundColor={"#000"} barStyle="light-content"/>
+        <Text>Login2!</Text>        
+        <LoginScreen></LoginScreen>
       </View>
     );
   }
 }
+
+const MobileFlashCardStatusBar = ({ backgroundColor, ...props }) => {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  );
+ };
 
 const styles = StyleSheet.create({
   container: {
